d212e46bf40a2f1ead8a7c2b673fac7eff49403a
Azamat Qasimov <azamatqsimov@gmail.com>
2025-01-30 12:12:50 -0800

    hel

--- main.js
+++ main.js
@@ -1,56 +1,58 @@
-// Write a function that changes the values ​​of two variables
+// // Write a function that changes the values ​​of two variables
 
-function numbesr(a, b) {
-  let temp = a;
-  a = b;
-  b = temp;
+// function numbesr(a, b) {
+//   let temp = a;
+//   a = b;
+//   b = temp;
 
-  console.log("change values: ", a, b);
-}
-numbesr(10, 5);
-// Write a function that checks whether a number is odd or even.
+//   console.log("change values: ", a, b);
+// }
+// numbesr(10, 5);
+// // Write a function that checks whether a number is odd or even.
 
-function eveOdd(num) {
-  if (num % 2 === 0) {
-    console.log("even");
-  } else {
-    console.log("odd");
-  }
-}
+// function eveOdd(num) {
+//   if (num % 2 === 0) {
+//     console.log("even");
+//   } else {
+//     console.log("odd");
+//   }
+// }
 
-eveOdd(6);
-eveOdd(3);
+// eveOdd(6);
+// eveOdd(3);
 
-// reverse text
+// // reverse text
 
-function change(textss) {
-  return textss.split("").reverse().join("");
-}
-let c = change("helloworld");
-console.log(c);
+// function change(textss) {
+//   return textss.split("").reverse().join("");
+// }
+// let c = change("helloworld");
+// console.log(c);
 
-// 4
-function findMinMax(arr) {
-  let max = arr[0];
-  let min = arr[0];
+// // 4
+// function findMinMax(arr) {
+//   let max = arr[0];
+//   let min = arr[0];
 
-  for (let a = 1; a <= arr.length; a++) {
-    if (arr[a] < min) min = arr[a];
-    if (arr[a] > max) max = arr[a];
-  }
+//   for (let a = 1; a <= arr.length; a++) {
+//     if (arr[a] < min) min = arr[a];
+//     if (arr[a] > max) max = arr[a];
+//   }
 
-  return { min, max };
-}
+//   return { min, max };
+// }
 
-console.log(findMinMax([3, 7, 1, 9, 2]));
+// console.log(findMinMax([3, 7, 1, 9, 2]));
 
-// second  4
+// // second  4
 
-function findMinMax2(arr) {
-  let max = Math.max(...arr);
-  let min = Math.min(...arr);
+// function findMinMax2(arr) {
+//   let max = Math.max(...arr);
+//   let min = Math.min(...arr);
 
-  return { min, max };
-}
+//   return { min, max };
+// }
 
-console.log(findMinMax2([-3, -7, -1, -9, -2]));
+// console.log(findMinMax2([-3, -7, -1, -9, -2]));
+
+// //! array tasklar start
